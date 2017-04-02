b5362d3e626cc6855d13d95e73de23d94b9bfd52
Jason <jason.dewitt@10up.com>
2017-04-02 15:35:41 -0500

    first unit test pass and url mapping, dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,3 +1,6 @@
+from lists import views
+from django.conf.urls import url
+
 """superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,5 +20,6 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name="home"),
+    #url(r'^admin/', admin.site.urls),
 ]
