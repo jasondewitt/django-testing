1ebe1e84ede55b399738b1979dac56703c00aaf5
Jason DeWitt <jason.dewitt@10up.com>
2017-04-13 15:32:32 -0500

    moved duplicate validtion into the form rather than on item save

--- lists/forms.py
+++ lists/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from lists.models import Item
+from django.core.exceptions import ValidationError
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
 DUPLICATE_ITEM_ERROR = "You've already got this in your list"
@@ -28,9 +29,12 @@ class ExistingListItemForm(ItemForm):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
 
-        def validate_unique(self):
-            try:
-                self.instance.validate_unique()
-            except:
-                e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
-                self._update_errors(e)
+    def save(self):
+        return forms.models.ModelForm.save(self)
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            self._update_errors(e)
